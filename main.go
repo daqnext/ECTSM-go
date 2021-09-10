0f98646fce1550e48f0ef1bc962d7716c7a20aca
leo <leo@leodeMacBook-Pro.local>
2021-09-10 14:16:59 +0800

    update

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+
+	"github.com/daqnext/ECTSM-go/utils"
+)
+
+func main() {
+
+	//////// first  part /////////////////////////
+
+	prikey_gen, _ := utils.GenSecp256k1KeyPair()
+	prikey_gen_string := utils.PrivateKeyToString(prikey_gen)
+	pubkey_gen_string := utils.PublicKeyToString(&prikey_gen.PublicKey)
+	fmt.Println("privatekey generated:", prikey_gen_string)
+	fmt.Println("publickey generated:", pubkey_gen_string)
+
+	//////// second part /////////////////////////
+
+	///////encrypt by public key//////////
+	rawMsg := "hello world"
+	pubKeyStr := "BMveUrioxvhfjsJ+WqkwXRwpgm+NPwEOFlXPAkhW4+HrI7kMEuklEJjolFQjSBLYDQ76e050fQjybfvAofHtf8M="
+	pubkeyfromstring, _ := utils.StringToPublicKey(pubKeyStr)
+	encryptmsg, _ := utils.ECCEncrypt(pubkeyfromstring, []byte(rawMsg))
+	base64msg := base64.StdEncoding.EncodeToString(encryptmsg)
+	fmt.Println("encryptmsg to base64:", base64msg)
+
+	////////decrypt by private key//////////
+	prikey_string := "To6r0h//zNLAvl/NuS3KPmUhURJOJCt4hOb8K+/7R3s="
+	prikeyfromstring, _ := utils.StringToPrivateKey(prikey_string)
+	encryptmsgfrombase64, _ := base64.StdEncoding.DecodeString(base64msg)
+	decryptmsg, _ := utils.ECCDecrypt(prikeyfromstring, encryptmsgfrombase64)
+	fmt.Println("decrypted raw msg :", string(decryptmsg))
+
+}
---
+++ utils/ecc.go
@@ -0,0 +1,81 @@
+package utils
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"encoding/base64"
+	"errors"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common/math"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/crypto/ecies"
+)
+
+func GenSecp256k1KeyPair() (*ecdsa.PrivateKey, error) {
+	return ecdsa.GenerateKey(crypto.S256(), rand.Reader)
+}
+
+func PublicKeyToString(pub *ecdsa.PublicKey) string {
+	priKeyByte := elliptic.Marshal(crypto.S256(), pub.X, pub.Y)
+	return base64.StdEncoding.EncodeToString(priKeyByte)
+}
+
+func PrivateKeyToString(priv *ecdsa.PrivateKey) string {
+	pubKeyByte := math.PaddedBigBytes(priv.D, priv.Params().BitSize/8)
+	return base64.StdEncoding.EncodeToString(pubKeyByte)
+}
+
+func StringToPrivateKey(stringd string) (*ecdsa.PrivateKey, error) {
+	d, err := base64.StdEncoding.DecodeString(stringd)
+	if err != nil {
+		return nil, err
+	}
+	priv := new(ecdsa.PrivateKey)
+	priv.PublicKey.Curve = crypto.S256()
+	if 8*len(d) != priv.Params().BitSize {
+		return nil, errors.New("invalid length")
+	}
+	priv.D = new(big.Int).SetBytes(d)
+
+	if priv.D.Sign() <= 0 {
+		return nil, errors.New("zero or negative")
+	}
+
+	priv.PublicKey.X, priv.PublicKey.Y = priv.PublicKey.Curve.ScalarBaseMult(d)
+	if priv.PublicKey.X == nil {
+		return nil, errors.New("invalid private key")
+	}
+	return priv, nil
+}
+
+func StringToPublicKey(pub string) (*ecdsa.PublicKey, error) {
+	if len(pub) == 0 {
+		return nil, errors.New("input error")
+	}
+	pubkeyrawstr, err := base64.StdEncoding.DecodeString(pub)
+	if err != nil {
+		return nil, errors.New("wrong input")
+	}
+
+	x, y := elliptic.Unmarshal(crypto.S256(), pubkeyrawstr)
+	return &ecdsa.PublicKey{Curve: crypto.S256(), X: x, Y: y}, nil
+}
+
+func ECCEncrypt(ecdsaPublicKey *ecdsa.PublicKey, rawMsg []byte) ([]byte, error) {
+	publicKey := ecies.ImportECDSAPublic(ecdsaPublicKey)
+	if publicKey.X == nil || publicKey.Y == nil {
+		return nil, errors.New("invalid key")
+	}
+	ct, err := ecies.Encrypt(rand.Reader, publicKey, rawMsg, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	return ct, nil
+}
+
+func ECCDecrypt(prik *ecdsa.PrivateKey, ct []byte) ([]byte, error) {
+	ecies_prv2 := ecies.ImportECDSA(prik)
+	return ecies_prv2.Decrypt(ct, nil, nil)
+}
