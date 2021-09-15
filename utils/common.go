381de3fbdf26bad3c09dffccaab5375aaee1f0b6
goodboy3 <zhangzhengbo@hotmail.com>
2021-09-15 11:20:24 +0800

    commit

--- utils/common.go
+++ utils/common.go
@@ -1,7 +1,6 @@
 package utils
 
 import (
-	"encoding/json"
 	"math/rand"
 )
 
@@ -12,23 +11,4 @@ func GenRandomKey() string {
 		b[i] = letterRunes[rand.Intn(len(letterRunes))]
 	}
 	return string(b)
-}
-
-func InterfaceToByte(obj interface{}) ([]byte, error) {
-	switch obj.(type) {
-	case string:
-		return []byte(obj.(string)), nil
-	case []byte:
-		return obj.([]byte), nil
-	default:
-		if obj == nil {
-			return nil, nil
-		} else {
-			objStringByte, err := json.Marshal(obj)
-			if err != nil {
-				return nil, err
-			}
-			return objStringByte, nil
-		}
-	}
 }
